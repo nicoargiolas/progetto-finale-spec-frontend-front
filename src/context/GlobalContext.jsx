e36db22c83a6dc737f75dd872832c5495db60e43
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-30 15:36:39 +0200

    Impostazione progetto e fetch getPlayers

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+
+const GlobalContext = createContext();
+
+export default function GlobalProvider({ children }) {
+    const [players, setPlayers] = useState([]);
+
+    async function fetchJson(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data
+    }
+
+    async function getPlayers() {
+        try {
+            const response = await fetchJson(`${import.meta.env.VITE_API_URL}/players`);
+            console.log(`Risposta`, response);
+            setPlayers(response)
+        } catch (error) {
+            throw new Error(`Impossibile recuperare i giocatori`)
+        }
+    }
+
+    useEffect(() => {
+        getPlayers();
+    }, []);
+
+    useEffect(() => {
+        console.log("Giocatori aggiornati", players);
+    }, [players])
+
+    return (
+        <GlobalContext.Provider value={{ players }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
